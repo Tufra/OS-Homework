86a4848927a44a5e4e3b4e48f1738b9d33eb2596
Tufra <60471036+Tufra@users.noreply.github.com>
2022-10-02 12:39:04 +0300

    sss

---
+++ week05/ex3.sh
@@ -0,0 +1,7 @@
+gcc ex3.c -o ex3
+
+time ./ex3 10000000 1 > ex3.txt
+time ./ex3 10000000 2 > ex3.txt
+time ./ex3 10000000 4 > ex3.txt
+time ./ex3 10000000 10 > ex3.txt
+time ./ex3 10000000 100 > ex3.txt
---
+++ week05/ex4.sh
@@ -0,0 +1,7 @@
+gcc ex4.c -o ex4
+
+time ./ex4 10000000 1 > ex4.txt
+time ./ex4 10000000 2 > ex4.txt
+time ./ex4 10000000 4 > ex4.txt
+time ./ex4 10000000 10 > ex4.txt
+time ./ex4 10000000 100 > ex4.txt
