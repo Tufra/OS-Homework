b372900f5a14c0ce080062e593346e97d0829098
Tufra <60471036+Tufra@users.noreply.github.com>
2022-10-25 18:22:42 +0300

    Update ex5.c

--- week07/ex5.c
+++ week07/ex5.c
@@ -2,10 +2,10 @@
 
 int main () {
     char **s;
-    char foo [] = "Hello World";
-    *s = foo;
-    printf ( "s is %s\n", *s);
-    s[0] = foo;
-    printf ("s[0] is %s\n", s[0]) ;
+    char foo[] = "Hello World";
+    s = &foo;
+    printf ( "s is %s\n", s);
+    s[0] = *foo;
+    printf ("s[0] is %s\n", s) ;
     return 0;
-}
+    }
